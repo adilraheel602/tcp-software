ed94d4663bef5b03605121f0328afcd9569e2ab0
M. Adil <adilraheel602@gmail.com>
2025-03-19 16:51:37 +0500

    fixed image issue

--- src/components/ReviewsCarousel.jsx
+++ src/components/ReviewsCarousel.jsx
@@ -3,7 +3,7 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import { Autoplay, Pagination } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/pagination";
-import quote from "../Images/quote.png";
+import quote from "../Images/Quote.png";
 
 const reviews = [
   {
